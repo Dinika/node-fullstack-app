596150ce70af306ec1e64d0a7868608198a96d09
Dinika <dinikasaxenas@gmail.com>
2019-09-26 23:29:24 +0200

    add routes
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('In middleware')
-  next()
+app.use('/about', (req, res, next) => {
+  console.log('Middleware 1')
+  res.send('<h1>Hi I am Dinika</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/work', (req, res, next) => {
+  console.log('For now')
+  res.send('<h1>I work at Minna</h1>')
+})
+
+app.use('/', (req, res, next) => {
   console.log('In other one')
   res.send('<h1>Corals!</h1>')
 })
