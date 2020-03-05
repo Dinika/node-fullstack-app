71313d8b94a60f325d733db4edbd3c1673c2f94b
Dinika <dinikasaxenas@gmail.com>
2020-03-06 00:41:59 +0100

    Handle page not found
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(adminRoutes)
 app.use(cafeRoutes)
+app.use('/', function (req, res, next) {
+  res.send('<h1>Page not found</h1>')
+})
 
 app.listen(4000)
