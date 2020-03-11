1d59f53db4fa454e4bafd6ebc92b2f73439a69da
Dinika <dinikasaxenas@gmail.com>
2020-03-11 23:34:14 +0100

    Retrieve data from db
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const cafeRoutes = require('./routes/cafe')
 const rootDir = require('./utilities/rootDir')
 const path = require('path')
 const page404Controller = require('./controllers/error')
+const db = require('./utilities/database')
 
 const app = express()
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
