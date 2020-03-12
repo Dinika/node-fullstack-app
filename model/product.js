4d6e6d7f02a0a2aac70bbbf487564f408dd10ca9
Dinika <dinikasaxenas@gmail.com>
2020-03-12 22:29:58 +0100

    Define product model in sequelize
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- model/product.js
+++ model/product.js
@@ -1,71 +1,102 @@
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../utilities/rootDir')
-const Cart = require('./cart')
-const db = require('../utilities/database')
+const Sequelize = require('sequelize')
+const sequelize = require('../utilities/database')
 
-const p = path.join(
-  rootDir,
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-module.exports = class Product {
-  constructor(name, price, imageUrl, description, id) {
-    this.id = id || Math.random().toString();
-    this.name = name;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
+const Product = sequelize.define(
+  'product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+}
+)
 
-  /*
-  * Updates existing product or inserts new product if none exists
-  */
-  upsert() {
-    return db.execute(
-      'INSERT INTO products (name, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.name, this.price, this.imageUrl, this.description]
-    )
-  }
 
-  static delete(id) {
-    getProductsFromFile(products => {
-      let productPrice
-      const updatedProducts = products.filter(p => {
-        if (p.id === id) {
-          productPrice = p.price
-        }
-        return p.id !== id
-      })
+// const fs = require('fs');
+// const path = require('path');
+// const rootDir = require('../utilities/rootDir')
+// const Cart = require('./cart')
+// const db = require('../utilities/database')
 
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (err) {
-          console.log(err)
-        }
-        else {
-          Cart.deleteProduct(id, productPrice)
-        }
-      })
-    })
-  }
+// const p = path.join(
+//   rootDir,
+//   'data',
+//   'products.json'
+// );
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
 
-  static findProductById(id, cb) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-  }
+// module.exports = class Product {
+//   constructor(name, price, imageUrl, description, id) {
+//     this.id = id || Math.random().toString();
+//     this.name = name;
+//     this.price = price;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//   }
 
-};
+//   /*
+//   * Updates existing product or inserts new product if none exists
+//   */
+//   upsert() {
+//     return db.execute(
+//       'INSERT INTO products (name, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+//       [this.name, this.price, this.imageUrl, this.description]
+//     )
+//   }
+
+//   static delete(id) {
+//     getProductsFromFile(products => {
+//       let productPrice
+//       const updatedProducts = products.filter(p => {
+//         if (p.id === id) {
+//           productPrice = p.price
+//         }
+//         return p.id !== id
+//       })
+
+//       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+//         if (err) {
+//           console.log(err)
+//         }
+//         else {
+//           Cart.deleteProduct(id, productPrice)
+//         }
+//       })
+//     })
+//   }
+
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM products')
+//   }
+
+//   static findProductById(id, cb) {
+//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
+//   }
+
+// };
