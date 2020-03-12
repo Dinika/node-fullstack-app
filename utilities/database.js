d398ef45cc4502a51f379afeba8850ba2e916ebe
Dinika <dinikasaxenas@gmail.com>
2020-03-12 22:22:51 +0100

    Use sequelize to query db
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- utilities/database.js
+++ utilities/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node_complete',
-  password: 'MPsdshdc@174'
-})
+const sequelize = new Sequelize(
+  'node-complete',
+  'root',
+  'MPsdshdc@174',
+  {
+    dialect: 'mysql',
+    host: 'localhost'
+  }
+)
 
-module.exports = pool.promise()
+module.exports = sequelize
