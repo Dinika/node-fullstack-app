81552ff6a1d6f9ff3304e342277fb0678e8d097b
Dinika <dinikasaxenas@gmail.com>
2020-03-12 23:21:04 +0100

    Update editted product in db
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,12 +6,17 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const isEditMode = req.query.edit
-  Product.findProductById(req.params.productId, product => {
-    if (!product) {
-      return res.status(404).redirect('/404')
-    }
-    res.render('admin/edit-product', { path: '', pageTitle: 'Edit product', product: product, editMode: true })
-  })
+  Product
+    .findByPk(req.params.productId)
+    .then(product => {
+      if (!product) {
+        return res.status(404).redirect('/404')
+      }
+      res.render('admin/edit-product', { path: '', pageTitle: 'Edit product', product: product, editMode: true })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -33,23 +38,29 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.postEditProduct = (req, res, next) => {
   const productId = req.params.productId
-  Product.findProductById(productId, (product) => {
-    // If no product found, redirect to 404 page
-    if (!product) {
-      return res.status(404).redirect('/404')
-    }
-    // Else update the product
-    // Destructuring would not work if the 'name' attribute of the html element is not the same as 
-    // the corresponding key name in the product model
-    const { name, price, imageUrl, description } = req.body
-    const incomingUpdatedProduct = new Product(name, price, imageUrl, description, productId)
-    incomingUpdatedProduct
-      .upsert()
-      .then(() => {
-        res.redirect('/admin/products')
-      })
-      .catch(err => console.log(err))
-  })
+  Product.findByPk(productId)
+    .then((product) => {
+      // If no product found, redirect to 404 page
+      if (!product) {
+        return res.status(404).redirect('/404')
+      }
+      // Else update the product
+      // Destructuring would not work if the 'name' attribute of the html element is not the same as 
+      // the corresponding key name in the product model
+      const { name, price, imageUrl, description } = req.body
+      product.name = name
+      product.price = price
+      product.imageUrl = imageUrl
+      product.description = description
+      return product.save()
+    })
+    .then(result => {
+      console.log(`Updated product with id: ${result.dataValues.id}`)
+      res.redirect('/admin/products')
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 exports.deleteProduct = (req, res, next) => {
