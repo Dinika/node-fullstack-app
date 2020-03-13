5dbd125bd29168b3cfafc3b195438622959dcf7f
Dinika <dinikasaxenas@gmail.com>
2020-03-13 08:31:26 +0100

    Create a dummmy user and pass it in requests
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ app.set('views', 'views')
 app.use(express.static(path.join(rootDir, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
 app.use(admin.router)
 app.use(cafeRoutes)
 app.use('/', page404Controller.get404)
@@ -25,8 +36,18 @@ app.use('/', page404Controller.get404)
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 User.hasMany(Product)
 
-sequelize.sync({ force: true })
+sequelize.sync()
   .then(result => {
+    return User.findByPk(1)
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'David', email: 'david@dcaro.es', password: 'password' })
+    }
+    return user
+  })
+  .then(user => {
+    console.log(user)
     app.listen(4000)
   })
   .catch(err => {
