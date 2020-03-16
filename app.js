10944cd0983bf1842a23349ec396b314ab19deee
Dinika <dinikasaxenas@gmail.com>
2020-03-16 23:36:39 +0100

    Define user model
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
-//const User = require('./model/user')
+const User = require('./model/user')
 const cafeRoutes = require('./routes/cafe')
 const rootDir = require('./utilities/rootDir')
 const path = require('path')
@@ -17,22 +17,37 @@ app.set('views', 'views')
 
 app.use(express.static(path.join(rootDir, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
-// app.use((req, res, next) => {
-//   User.findById('5e6e24f61a023b4bdf9ddb4d')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// })
+app.use((req, res, next) => {
+  User.findById('5e6ffef56ae68b3310f0d465')
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 app.use(admin.router)
 app.use(cafeRoutes)
 app.use('/', page404Controller.get404)
 
 mongoose.connect(connectionUri)
   .then(result => {
+    return User.findOne()
+  })
+  .then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'Dinika',
+        email: 'dinikasaxenas@gmail.com',
+        cart: {
+          items: []
+        }
+      })
+      user.save()
+    }
+  })
+  .then(() => {
     app.listen(4000)
   })
   .catch(err => {
