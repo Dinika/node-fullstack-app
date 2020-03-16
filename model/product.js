446779c71a5e7d6e7c7e6cdf9da1746f8e95b298
Dinika <dinikasaxenas@gmail.com>
2020-03-16 22:51:35 +0100

    Define product model
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- model/product.js
+++ model/product.js
@@ -1,3 +1,36 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
 // const getDB = require('../utilities/database').getDB
 // const mongoDb = require('mongodb')
 
