1187de704a298929f09240b0841ae58734716449
Dinika <dinikasaxenas@gmail.com>
2020-03-22 16:30:14 +0100

    Save user email instead of name of orders
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/cafe.js
+++ controllers/cafe.js
@@ -91,7 +91,7 @@ exports.checkout = (req, res, next) => {
       })
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: productsInCart
