6f486a2f136a7f4b80a4730887b1752a2e4c062d
Dinika <dinikasaxenas@gmail.com>
2020-03-23 11:57:31 +0100

    Add custom input validation for email
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- routes/authentication.js
+++ routes/authentication.js
@@ -8,7 +8,15 @@ router.post('/login', authController.postLogin)
 router.post('/logout', authController.postLogout)
 router.get('/signup', authController.getSignup)
 router.post('/signup',
-  check('email').isEmail().withMessage('Invalid e-mail'),
+  check('email')
+    .isEmail()
+    .withMessage('Invalid e-mail')
+    .custom((value, { req }) => {
+      if (value === 'test@test.com') {
+        throw new Error('This email address is forbidden')
+      }
+      return true
+    }),
   authController.postSignup)
 router.get('/reset-password', authController.getResetPassword)
 router.post('/reset-password', authController.postResetPassword)
