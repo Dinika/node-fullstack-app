0d138f23b3ea2c5efb59e1f0f14bb3af51dd409c
Dinika <dinikasaxenas@gmail.com>
2020-03-23 12:03:08 +0100

    Add input validation for password
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authentication')
-const { check } = require('express-validator/check')
+const { check, body } = require('express-validator/check')
 
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.post('/logout', authController.postLogout)
 router.get('/signup', authController.getSignup)
 router.post('/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Invalid e-mail')
-    .custom((value, { req }) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address is forbidden')
-      }
-      return true
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Invalid e-mail')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address is forbidden')
+        }
+        return true
+      }),
+    body('password', 'Password should be atleast 6 character long and cannot have special characters')
+      .isLength({ min: 6 })
+      .isAlphanumeric()
+  ],
   authController.postSignup)
 router.get('/reset-password', authController.getResetPassword)
 router.post('/reset-password', authController.postResetPassword)
